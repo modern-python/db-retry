0c27f6fee76391c144d65ea801fdfee547a839b4
Artur Shiriev <lesnik512@gmail.com>
2026-06-26 22:20:50 +0300

    refactor: wire postgres_retry through is_retriable seam

--- db_retry/retry.py
+++ db_retry/retry.py
@@ -2,34 +2,19 @@ import functools
 import logging
 import typing
 
-import asyncpg
 import tenacity
-from sqlalchemy.exc import DBAPIError
 
 from db_retry import settings
+from db_retry.retriable import is_retriable
 
 
 logger = logging.getLogger(__name__)
 
 
-def _is_retriable_dbapi_error(exception: BaseException) -> bool:
-    return (
-        isinstance(exception, DBAPIError)
-        and exception.orig is not None
-        and isinstance(exception.orig.__cause__, (asyncpg.SerializationError, asyncpg.PostgresConnectionError))
-    )
-
-
-def _retry_handler(exception: BaseException) -> bool:
-    current: BaseException | None = exception
-    seen: set[int] = set()
-    while current is not None and id(current) not in seen:
-        seen.add(id(current))
-        if _is_retriable_dbapi_error(current):
-            logger.debug("postgres_retry, retrying")
-            return True
-        current = current.__cause__ or current.__context__
-
+def _log_and_decide(exception: BaseException) -> bool:
+    if is_retriable(exception):
+        logger.debug("postgres_retry, retrying")
+        return True
     logger.debug("postgres_retry, giving up on retry")
     return False
 
@@ -57,7 +42,7 @@ def postgres_retry[**P, T](
             retryer = tenacity.AsyncRetrying(
                 stop=tenacity.stop_after_attempt(retries if retries is not None else settings.get_retries_number()),
                 wait=tenacity.wait_exponential_jitter(),
-                retry=tenacity.retry_if_exception(_retry_handler),
+                retry=tenacity.retry_if_exception(_log_and_decide),
                 reraise=True,
                 before=tenacity.before_log(logger, logging.DEBUG),
             )
